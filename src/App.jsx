9f2bce4b4adef68edf778547190489338c0a9425
Garrett Murphy <garrettmurphy.phx@gmail.com>
2023-12-26 07:54:41 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Nav from "./components/nav";
+
+import "./scss/index.scss";
+
+function App() {
+  return (
+    <div>
+      <Nav />
+    </div>
+  );
+}
+
+export default App;
